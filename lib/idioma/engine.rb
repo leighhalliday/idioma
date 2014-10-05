dded5df1af0e223acda99b3618ee7f5ddefe02bc
Leigh Halliday <lhalliday@bettertheworld.com>
2014-10-05 19:33:18 -0400

    So that the main app runs our migrations

--- lib/idioma/engine.rb
+++ lib/idioma/engine.rb
@@ -2,6 +2,14 @@ module Idioma
   class Engine < ::Rails::Engine
     isolate_namespace Idioma
 
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
+
     config.generators do |g|
       g.test_framework      :rspec,        :fixture => false
       g.fixture_replacement :factory_girl, :dir => 'spec/factories'
